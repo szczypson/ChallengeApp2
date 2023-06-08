4bc9c43a978b12645306af5807b770bd2cb7921d
szczypson <przemyslaw.szczypek@gmail.com>
2023-06-08 11:45:20 +0200

    Zadanie domowe dzien 4 inicjalizacja.

---
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Napisz w Program.cs kod, w którym 
+// zadeklarujesz zmienną z imieniem
+// zmienną z płcią (dobrać zmienną)
+// zweryfikujesz dane i wyswietlisz kedem z komunikatow:
+// (1): "Kobieta poniżj 30 lat"
+// (2): "Ewa, lat 33"
+// (3): "Niepełnoletni mężczyzna"
+// --------------------------------
+// (mozna dodac swoje)
+
+// na koncu CTRL K D
+// save file, COMMIT stage, push....hub
